61b155a3dea9a8537fab9ba184e3f980cbe05645
anggi1234 <anggirestuoerdana@gmail.com>
2023-07-17 13:40:54 +0700

    update vlogin

--- application/modules/login/views/vlogin.php
+++ application/modules/login/views/vlogin.php
@@ -9,6 +9,25 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
+    <!-- Custom styles for this template -->
+    <link href="<?= base_url(); ?>/assets/dist/css/signin.css" rel="stylesheet">
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+
     <title>Login SiJaspel</title>
 </head>
 
@@ -26,25 +45,46 @@
     <div class="container mt-5 pt-5">
         <div class="row">
             <div class="col-12 col-sm-8 col-md-6 m-auto">
-                <div class="card border-0 shadow">
+                <div class="card border-0 bacground-0">
                     <div class="card-body">
-                        <svg class="mx-auto my-3" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                        </svg>
-                        <form action="">
-                            <input type="text" name="" id="" class="form-control my-3 py-2" placeholder="Username">
-                            <input type="text" name="" id="" class="form-control my-3 py-2" placeholder="Password">
-                            <div class="text-center mt-3">
-                                <button class="btn btn-primary">Login</button>
-                            </div>
-                        </form>
+                        <main class="form-signin">
+                            <form>
+                                <img class="mb-4" src="<?= base_url(); ?>/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+                                <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+                                <div class="form-floating" style="">
+                                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                                    <label for="floatingInput">Email address</label>
+                                </div>
+                                <div class="form-floating">
+                                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                                    <label for="floatingPassword">Password</label>
+                                </div>
+
+                                <div class="checkbox mb-3">
+                                    <label>
+                                        <input type="checkbox" value="remember-me"> Remember me
+                                    </label>
+                                </div>
+                                <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+                                <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+                            </form>
+                        </main>
+
                     </div>
                 </div>
+
             </div>
         </div>
+
     </div>
 
+
+
+
 </body>
+
+
+
 
 </html>
