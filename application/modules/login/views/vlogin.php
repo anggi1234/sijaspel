146422bb3747db45c38f8a61a27acf711fd96d48
anggi1234 <anggirestuoerdana@gmail.com>
2023-07-23 07:51:32 +0700

    update login

--- application/modules/login/views/vlogin.php
+++ application/modules/login/views/vlogin.php
@@ -50,11 +50,11 @@
             <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
             <div class="form-floating">
-                <input type="text" class="form-control" id="floatingInput" placeholder="Username">
+                <input type="text" name="username" class="form-control" id="floatingInput" placeholder="Username">
                 <label for="floatingInput">Username</label>
             </div>
             <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password">
                 <label for="floatingPassword">Password</label>
             </div>
 
@@ -63,6 +63,7 @@
                     <input type="checkbox" value="remember-me"> Remember me
                 </label>
             </div>
+            <?= $this->session->flashdata('error') ? $this->session->flashdata('error') : ''; ?>
             <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
             <p class="mt-5 mb-3 text-muted">RSUD Panti Nugroho &copy; <?= date("Y"); ?></p>
         </form>
