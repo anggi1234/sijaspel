960ac8c7706ef836a1b0809441b68af85ec45618
anggi1234 <anggirestuoerdana@gmail.com>
2023-07-26 09:14:35 +0700

    add model lapjaspel

--- application/modules/lapjaspel/models/Mlaporan.php
+++ application/modules/lapjaspel/models/Mlaporan.php
@@ -6,52 +6,105 @@ class Mlaporan extends CI_Model
 
     public function getLaporanSemua($tglmulai, $tglselesai)
     {
-        return $this->db->query("SELECT MasterTindakan.NAMAPMR, Sum(TransPMR.JmlPmr) AS JML, Sum(MasterTindakan.JPPL*TransPMR.JasaPelayanan) AS JM
-        FROM TransPMR INNER JOIN MasterTindakan ON TransPMR.KodePMR = MasterTindakan.KODEPMR
-        WHERE TransPMR.TglPMR between '$tglmulai' and '$tglselesai'
-        GROUP BY MasterTindakan.NAMAPMR 
-        order by MasterTindakan.NAMAPMR
-      ")->result_array();
+        return $this->db->query("SELECT
+            MasterTindakan.NAMAPMR,
+            SUM ( TransPMR.JmlPmr ) AS JML,
+            SUM ( MasterTindakan.JPPL* TransPMR.JasaPelayanan ) AS JM 
+        FROM
+            TransPMR
+            INNER JOIN MasterTindakan ON TransPMR.KodePMR = MasterTindakan.KODEPMR 
+        WHERE
+            TransPMR.TglPMR BETWEEN '$tglmulai' 
+            AND '$tglselesai' 
+        GROUP BY
+            MasterTindakan.NAMAPMR 
+        ORDER BY
+            MasterTindakan.NAMAPMR")->result_array();
     }
     public function getLaporanRekap($tglmulai, $tglselesai)
     {
-        return $this->db->query("select mj.Nama,isnull(k.jmlpmr,0) as jmlpmr, isnull(k.jp,0) as jmljaspel
-        from MasterPenjamin mj
-        left join (select sum(mt.JPPL*tp.JasaPelayanan) as jp, count(tp.KodePMR) as jmlpmr, tp.KodePT from TransPMR tp
-        left join MasterTindakan mt on mt.KODEPMR = tp.KodePMR
-        where tp.TglPMR between '$tglmulai' and '$tglselesai' and tp.DokterPeriksa='SP09'
-        group by tp.KodePT) k on k.KodePT = mj.KodePT
-        
-        where mj.Aktif='Y' order by mj.KodePT ")->result_array();
+        return $this->db->query("SELECT
+            mj.Nama,
+            isnull( k.jmlpmr, 0 ) AS jmlpmr,
+            isnull( k.jp, 0 ) AS jmljaspel 
+        FROM
+            MasterPenjamin mj
+            LEFT JOIN (
+            SELECT SUM
+                ( mt.JPPL* tp.JasaPelayanan ) AS jp,
+                COUNT ( tp.KodePMR ) AS jmlpmr,
+                tp.KodePT 
+            FROM
+                TransPMR tp
+                LEFT JOIN MasterTindakan mt ON mt.KODEPMR = tp.KodePMR 
+            WHERE
+                tp.TglPMR BETWEEN '$tglmulai' 
+                AND '$tglselesai' 
+                AND tp.DokterPeriksa= 'SP09' 
+            GROUP BY
+                tp.KodePT 
+            ) k ON k.KodePT = mj.KodePT 
+        WHERE
+            mj.Aktif= 'Y' 
+        ORDER BY
+            mj.KodePT")->result_array();
     }
 
     public function getLaporanDokter($tglmulai, $tglselesai, $dokter)
     {
-        return $this->db->query("SELECT MasterTindakan.NAMAPMR, Sum(TransPMR.JmlPmr) AS JML, Sum(MasterTindakan.JPPL*TransPMR.JasaPelayanan) AS JM
-        FROM TransPMR INNER JOIN MasterTindakan ON TransPMR.KodePMR = MasterTindakan.KODEPMR
-        WHERE TransPMR.TglPMR between '$tglmulai' and '$tglselesai' and TransPMR.DokterPeriksa = '$dokter'
-        GROUP BY MasterTindakan.NAMAPMR 
-        order by MasterTindakan.NAMAPMR
-      ")->result_array();
+        return $this->db->query("SELECT
+            MasterTindakan.NAMAPMR,
+            SUM ( TransPMR.JmlPmr ) AS JML,
+            SUM ( MasterTindakan.JPPL* TransPMR.JasaPelayanan ) AS JM 
+        FROM
+            TransPMR
+            INNER JOIN MasterTindakan ON TransPMR.KodePMR = MasterTindakan.KODEPMR 
+        WHERE
+            TransPMR.TglPMR BETWEEN '$tglmulai' 
+            AND '$tglselesai' 
+            AND TransPMR.DokterPeriksa = '$dokter' 
+        GROUP BY
+            MasterTindakan.NAMAPMR 
+        ORDER BY
+            MasterTindakan.NAMAPMR")->result_array();
     }
 
     public function getLaporanBagian($tglmulai, $tglselesai, $bagian)
     {
-        return $this->db->query("SELECT MasterTindakan.NAMAPMR, Sum(TransPMR.JmlPmr) AS JML, Sum(MasterTindakan.JPPL*TransPMR.JasaPelayanan) AS JM
-        FROM TransPMR INNER JOIN MasterTindakan ON TransPMR.KodePMR = MasterTindakan.KODEPMR
-        WHERE TransPMR.TglPMR between '$tglmulai' and '$tglselesai' and TransPMR.KodeBagian = '$bagian'
-        GROUP BY MasterTindakan.NAMAPMR 
-        order by MasterTindakan.NAMAPMR
-      ")->result_array();
+        return $this->db->query("SELECT
+            MasterTindakan.NAMAPMR,
+            SUM ( TransPMR.JmlPmr ) AS JML,
+            SUM ( MasterTindakan.JPPL* TransPMR.JasaPelayanan ) AS JM 
+        FROM
+            TransPMR
+            INNER JOIN MasterTindakan ON TransPMR.KodePMR = MasterTindakan.KODEPMR 
+        WHERE
+            TransPMR.TglPMR BETWEEN '$tglmulai' 
+            AND '$tglselesai' 
+            AND TransPMR.KodeBagian = '$bagian' 
+        GROUP BY
+            MasterTindakan.NAMAPMR 
+        ORDER BY
+            MasterTindakan.NAMAPMR")->result_array();
     }
 
     public function getLaporanBagDok($tglmulai, $tglselesai, $bagian, $dokter)
     {
-        return $this->db->query("SELECT MasterTindakan.NAMAPMR, Sum(TransPMR.JmlPmr) AS JML, Sum(MasterTindakan.JPPL*TransPMR.JasaPelayanan) AS JM
-        FROM TransPMR INNER JOIN MasterTindakan ON TransPMR.KodePMR = MasterTindakan.KODEPMR
-        WHERE TransPMR.TglPMR between '$tglmulai' and '$tglselesai' and TransPMR.KodeBagian = '$bagian' and TransPMR.DokterPeriksa = '$dokter'
-        GROUP BY MasterTindakan.NAMAPMR 
-        order by MasterTindakan.NAMAPMR
-      ")->result_array();
+        return $this->db->query("SELECT
+            MasterTindakan.NAMAPMR,
+            SUM ( TransPMR.JmlPmr ) AS JML,
+            SUM ( MasterTindakan.JPPL* TransPMR.JasaPelayanan ) AS JM 
+        FROM
+            TransPMR
+            INNER JOIN MasterTindakan ON TransPMR.KodePMR = MasterTindakan.KODEPMR 
+        WHERE
+            TransPMR.TglPMR BETWEEN '$tglmulai' 
+            AND '$tglselesai' 
+            AND TransPMR.KodeBagian = '$bagian' 
+            AND TransPMR.DokterPeriksa = '$dokter' 
+        GROUP BY
+            MasterTindakan.NAMAPMR 
+        ORDER BY
+            MasterTindakan.NAMAPMR")->result_array();
     }
 }
